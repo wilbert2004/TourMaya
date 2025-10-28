ebec18bc15927a0591653896c21d909975337fb2
Wilbert Oliver Chan Uc <131514400+wilbert2004@users.noreply.github.com>
2025-10-27 21:25:40 -0600

    terreno perdido
    ya corregido todo

---
+++ Assets/FantasyEnvironments/Environments/Town/Scripts/Door.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownDoor : MonoBehaviour
+{
+	public Animator anim;
+
+	// Use this for initialization
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		anim.SetBool("DoorOpen", true);
+		anim.SetBool("DoorClose", false);
+
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		anim.SetBool("DoorOpen", false);
+		anim.SetBool("DoorClose", true);
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
